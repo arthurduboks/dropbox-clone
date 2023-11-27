fb81bd0803f11c663e98d83091830ddab9ac45e7
Arthur Duboks <duboks.arthur@gmail.com>
2023-11-27 16:59:14 -0500

    setup clerk AUTH

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="">
       <h1>DropBox Clone</h1>
+      <UserButton />
     </main>
   );
 }
