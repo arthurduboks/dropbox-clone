53ff7fbf8c6db6cbc53ace1b67e91ee4ddd13637
Arthur Duboks <duboks.arthur@gmail.com>
2023-11-27 17:42:24 -0500

    install shadcn ui

---
+++ app/components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import dropBoxLogo from "@/public/dropbox-logo.png";
+import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="bg-[#0160FE] w-fit">
+          <Image src={dropBoxLogo} alt="Logo" height={50} width={50} />
+        </div>
+        <h1 className="font-bold text-xl">DropBox</h1>
+      </Link>
+
+      <div className="px-5 flex space-x-2 items-center">
+        {/* Color mode */}
+        <UserButton afterSignOutUrl="/" />
+
+        <SignedOut>
+          <SignInButton afterSignInUrl="dashboard" mode="modal" />
+        </SignedOut>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
